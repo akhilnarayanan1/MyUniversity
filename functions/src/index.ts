9d44a1b1b4b69bf56c94122045d90c56538e63c2
Akhil Narayanan <akhilnarayanan.work@gmail.com>
2022-01-12 19:41:53 +0530

    removing live endpoint

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -37,16 +37,16 @@ app.get("/:universityname", async (req, res) => {
 });
 
 
-app.get("/live/:universityname", async (req, res) => {
-  switch (req.params.universityname) {
-    case "rgpv":
-      // eslint-disable-next-line no-case-declarations
-      const [statusCode, rgpvNotifications] = await runRGPV();
-      return res.status(statusCode).json({"data": rgpvNotifications});
-    default:
-      return res.status(400).json({"message": "Invalid University Name"});
-  }
-});
+// app.get("/live/:universityname", async (req, res) => {
+//   switch (req.params.universityname) {
+//     case "rgpv":
+//       // eslint-disable-next-line no-case-declarations
+//       const [statusCode, rgpvNotifications] = await runRGPV();
+//       return res.status(statusCode).json({"data": rgpvNotifications});
+//     default:
+//       return res.status(400).json({"message": "Invalid University Name"});
+//   }
+// });
 
 exports.api = functions.https.onRequest(app);
 exports.api_scheduled = functions.pubsub.schedule("2 */1 * * *")
